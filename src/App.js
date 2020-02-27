3f63edf24b944efa91271365e60299aabc9b0adf
idong essien <idongessien@idong-essien.local>
2020-02-26 20:26:59 -0500

    imported useState hook and set up state property

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newMbr, setNewMbr] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-      
+    <div className="body">
+      <header>
+        <h1>#AwesomeDevTeam</h1>
       </header>
+      <footer>
+        <p>Copyright &copy; {new Date().getFullYear()} #AwesomeDevTeam. All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/TeamInfo.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import teammates from '../teammates.js';
+
+function TeamInfo() {
+    const [theTeam, setTheTeam] = useState([]);
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default TeamInfo;
